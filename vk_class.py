8a615c140b612517beff6f2a84cb93b842e60a1d
dzaytsev89 <dzaytsev89@gmail.com>
2022-07-11 11:16:53 +0300

    - add tokens.ini - def moved into classes

--- vk_class.py
+++ vk_class.py
@@ -7,63 +7,47 @@ class VK:
 
     def __init__(self, vk_token, user_id, version='5.131'):
         self.token = vk_token
-        self.id = user_id
+        self.vk_id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
     def users_info(self):
-        params = {'user_ids': self.id}
+        params = {'user_ids': self.vk_id}
         response = requests.get(self.url + 'users.get', params={**self.params, **params})
         return response.json()
 
-    def photos_get_profile(self):
-        params = {'album_id': 'profile', 'extended': '1', 'owner_id': self.id}
-        req = requests.get(self.url + 'photos.get', params={**self.params, **params}).json()
-        # print(req['response'])
-        avatar_albums = req['response']['items']
-        return avatar_albums
-
     def photos_get(self, album_id):
-        params = {'album_id': album_id, 'extended': '1', 'owner_id': self.id}
+        params = {'album_id': album_id, 'extended': '1', 'owner_id': self.vk_id}
         req = requests.get(self.url + 'photos.get', params={**self.params, **params}).json()
-        # print(req['response'])
-        avatar_albums = req['response']['items']
-        return avatar_albums
+        photo_albums = req['response']['items']
+        return photo_albums
 
-    def largest_avatars(self):
-        avatars = self.photos_get_profile()
+    def largest_photo(self, album_type):
+        photos = self.photos_get(album_type)
         id_photos = []
-        for album in avatars:
+        for album in photos:
             photo_cache = dict()
             photo_cache['id'] = str(album['id'])
             photo_cache['likes'] = str(album['likes']['count'])
             photo_cache['date'] = datetime.datetime.fromtimestamp(album['date']).strftime('%Y-%m-%d_%H-%M-%S')
-            # print(sorted(album['sizes'], key=lambda type: type['type']))
             for size in album['sizes']:
                 if size['type'] == 'w':
                     photo_cache['largest_url'] = size['url']
                     photo_cache['type'] = size['type']
                 else:
-                    photo_cache['largest_url'] = sorted(album['sizes'], key=lambda type: type['type'])[-1]['url']
+                    photo_cache['largest_url'] = sorted(album['sizes'], key=lambda type_: type_['type'])[-1]['url']
                     photo_cache['type'] = size['type']
             id_photos.append(photo_cache)
         return id_photos
 
-    def largest_foto(self, album_type):
-        avatars = self.photos_get(album_id=album_type)
-        id_photos = []
-        for album in avatars:
-            photo_cache = dict()
-            photo_cache['id'] = str(album['id'])
-            photo_cache['likes'] = str(album['likes']['count'])
-            photo_cache['date'] = datetime.datetime.fromtimestamp(album['date']).strftime('%Y-%m-%d_%H-%M-%S')
-            # print(sorted(album['sizes'], key=lambda type: type['type']))
-            for size in album['sizes']:
-                if size['type'] == 'w':
-                    photo_cache['largest_url'] = size['url']
-                    photo_cache['type'] = size['type']
-                else:
-                    photo_cache['largest_url'] = sorted(album['sizes'], key=lambda type: type['type'])[-1]['url']
-                    photo_cache['type'] = size['type']
-            id_photos.append(photo_cache)
-        return id_photos
+    def closed_vk_id(self):
+        status = self.users_info()
+        if status['response'][0]['can_access_closed']:
+            return True
+        else:
+            print('Профиль закрыт, невозможно скачать фото без соответствующего токена')
+            return False
+
+    def find_vk_id(self):
+        info = self.users_info()
+        return str(info['response'][0]['id'])
--- ya_disk.py
+++ ya_disk.py
@@ -1,4 +1,6 @@
 import requests
+import vk_class
+import time
 
 
 class YandexDisk:
@@ -35,7 +37,6 @@ class YandexDisk:
         headers = self.get_headers()
         params = {'path': path}
         response = requests.get(files_url, headers=headers, params=params)
-        # pprint(response.json())
         folders = response.json()['_embedded']['items']
         ids_folders = []
         for folder in folders:
@@ -49,27 +50,10 @@ class YandexDisk:
         params = {'path': path}
         response = requests.put(headers=headers, url=url, params=params)
         response.raise_for_status()
-        # pprint(response.json())
         if response.status_code == 201:
-            # print("Success - 201")
             pass
         else:
             print('Error! ', response.status_code)
-
-    # def _get_upload_link(self, disk_file_path):
-    #     upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-    #     headers = self.get_headers()
-    #     params = {"path": disk_file_path, "overwrite": "true"}
-    #     response = requests.get(upload_url, headers=headers, params=params)
-    #     # pprint(response.json())
-    #     return response.json()
-    #
-    # def upload_file_to_disk(self, disk_file_path, filename):
-    #     href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-    #     response = requests.put(href, data=open(filename, 'rb'))
-    #     response.raise_for_status()
-    #     if response.status_code == 201:
-    #         print("Success")
 
     def upload_file_from_url(self, file_url, disk_path):
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -82,3 +66,42 @@ class YandexDisk:
             print(f"File {disk_path.split('/')[-1]} uploaded!")
         else:
             print('Error!', response.status_code)
+
+    def check_dir_vk_id(self, vk_id, path):
+        vk_id_backup_folders = self.find_folders(path)
+        if vk_id not in vk_id_backup_folders:
+            self.create_dir(path + vk_id)
+            return print("Папка для записи фото создана")
+        else:
+            return print("Папка для записи фото существует")
+
+    def copy_vk_photo_to_ya(self, path, count, album_type, vk_client, photo_log):
+        counter = int(count)
+        photo_bank = vk_class.VK.largest_photo(vk_client, album_type=album_type)
+        print(photo_bank)
+        if len(photo_bank) < int(count):
+            print(f'Нет столько фотографий в альбоме, будет скопировано'
+                  f' {len(photo_bank)} фотографий')
+            counter = len(photo_bank)
+        self.check_dir_vk_id(vk_id=vk_client.vk_id, path=path)
+        # photo_log = dict()
+        # photo_log['photos'] = []
+        # pprint(sorted(photo_bank, key=lambda like: int(photo_bank['likes']))[::-1])
+        for photo in photo_bank:
+            if counter != 0:
+                file_name = photo['likes'] + '.jpeg'
+                if photo['likes'] + '.jpeg' in self.get_files_list_name(path + vk_client.vk_id):  # check_filename
+                    file_name = photo['likes'] + '_' + photo['date'] + '.jpeg'
+                some_dict = dict()
+                some_dict['file_name'] = file_name
+                some_dict['size'] = photo['type']
+                photo_log.append(some_dict)
+                file_path = path + vk_client.vk_id + '/' + file_name
+                self.upload_file_from_url(file_url=photo['largest_url'], disk_path=file_path)
+                time.sleep(2)
+            else:
+                print(f'Загружено {count} фотографий')
+                print(f'Информацию о загруженных фотографиях можно посмотреть'
+                      f' в файле loaded_photos_{vk_client.vk_id}.json')
+                return photo_log
+            counter -= 1
